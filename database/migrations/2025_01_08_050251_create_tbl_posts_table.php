50181e8fa16158ae6926e73c862222c020519e06
135Akbar <149138366+135Akbar@users.noreply.github.com>
2025-01-08 12:36:31 +0700

    2025_01_08_050251_create_tbl_posts_table.php

--- database/migrations/2025_01_08_050251_create_tbl_posts_table.php
+++ database/migrations/2025_01_08_050251_create_tbl_posts_table.php
@@ -9,19 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('tbl_posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->integer('hits')->default(0);
+            $table->enum('aktif', ['Y', 'N'])->default('Y');
+            $table->enum('status', ['publish', 'draft'])->default('draft');
             $table->timestamps();
+    
+            // Foreign key jika ada relasi dengan tabel user
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('tbl_posts');
-    }
-};
+}
